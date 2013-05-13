d9602848916b063b6d5a31937137313d3b3b76b3
romku <sromku@gmail.com>
2013-05-14 00:51:01 +0300

    - Sample app with JSON schema file

---
+++ SQLite Generator Test App/src/com/sromku/sqliteapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sromku.sqliteapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sromku.sqlitesandbox.R;
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+}
